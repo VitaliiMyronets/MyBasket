51b7cbde9b9837eee690c9d3d01ee8689a5ea885
Vitalii Myronets <mironetsvitaly@gmail.com>
2023-07-17 15:29:14 +0300

    reload repository with new project on spring boot

---
+++ src/main/java/ua/myronets/FirstWebApp/FirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package ua.myronets.FirstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebAppApplication.class, args);
+	}
+
+}
