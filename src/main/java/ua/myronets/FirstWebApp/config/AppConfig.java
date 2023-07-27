c1101262e0fb70e58f8c97002cd73907cd8ae310
Alexander Goltsov <el_amigo@ua.fm>
2023-07-27 13:07:55 +0300

    fix imports javax -> jakarta

--- src/main/java/ua/myronets/FirstWebApp/config/AppConfig.java
+++ src/main/java/ua/myronets/FirstWebApp/config/AppConfig.java
@@ -1,11 +1,12 @@
 package ua.myronets.FirstWebApp.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Configuration
+@EntityScan("ua.myronets.FirstWebApp")
+@EnableJpaRepositories("ua.myronets.FirstWebApp")
 public class AppConfig {
 
 }
--- src/main/java/ua/myronets/FirstWebApp/repo/PostRepository.java
+++ src/main/java/ua/myronets/FirstWebApp/repo/PostRepository.java
@@ -1,8 +1,7 @@
 package ua.myronets.FirstWebApp.repo;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ua.myronets.FirstWebApp.models.Post;
-@Repository
+
 public interface PostRepository extends CrudRepository<Post, Long> {
 }
