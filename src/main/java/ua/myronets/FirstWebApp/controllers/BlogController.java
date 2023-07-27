0a46ef729d9ecf1d1e395b62ac11b1eb95fb3b43
Vitalii Myronets <mironetsvitaly@gmail.com>
2023-07-27 18:05:03 +0300

    added the ability to create posts

--- src/main/java/ua/myronets/FirstWebApp/controllers/BlogController.java
+++ src/main/java/ua/myronets/FirstWebApp/controllers/BlogController.java
@@ -4,8 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.myronets.FirstWebApp.models.Post;
 import ua.myronets.FirstWebApp.repo.PostRepository;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -18,5 +24,27 @@ public class BlogController {
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
         return "blog-main";
+    }
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title,@RequestParam String anons,@RequestParam String fullText, Model model){
+        Post post = new Post(title, anons, fullText);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post", res);
+        return "blog-details";
     }
 }
