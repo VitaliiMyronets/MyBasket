e5c9e37b3671d791731546eabaf943ea4aa78670
Vitalii Myronets <mironetsvitaly@gmail.com>
2023-10-26 14:09:40 +0300

    lombok is added

--- src/main/java/ua/myronets/FirstWebApp/models/Post.java
+++ src/main/java/ua/myronets/FirstWebApp/models/Post.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@RequiredArgsConstructor
 @Entity
 public class Post {
     @Id
@@ -14,48 +18,6 @@ public class Post {
     private String title, anons, fullText;
     private int views;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAnons() {
-        return anons;
-    }
-
-    public void setAnons(String anons) {
-        this.anons = anons;
-    }
-
-    public String getFullText() {
-        return fullText;
-    }
-
-    public void setFullText(String fullText) {
-        this.fullText = fullText;
-    }
-
-    public int getViews() {
-        return views;
-    }
-
-    public void setViews(int views) {
-        this.views = views;
-    }
-
-    public Post() {
-    }
 
     public Post(String title, String anons, String fullText) {
         this.title = title;
