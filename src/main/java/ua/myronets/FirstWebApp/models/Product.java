0090d310bcf6ec0d7242e526fab00aba1c06ebcf
Vitalii Myronets <mironetsvitaly@gmail.com>
2023-10-26 17:02:58 +0300

    Product model added

--- src/main/java/ua/myronets/FirstWebApp/models/Product.java
+++ src/main/java/ua/myronets/FirstWebApp/models/Product.java
@@ -1,5 +1,26 @@
 package ua.myronets.FirstWebApp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Product {
-    long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    String nameOfProduct, unit;
+    float quantity;
+
+    public Product(String nameOfProduct, String unit, float quantity) {
+        this.nameOfProduct = nameOfProduct;
+        this.unit = unit;
+        this.quantity = quantity;
+    }
 }
