f983c201ba998c49d2b04871ab8a000adecadca5
Vitalii Myronets <mironetsvitaly@gmail.com>
2025-05-28 16:39:52 +0300

    repositories have been improved

--- src/main/java/ua/myronets/FirstWebApp/repo/ProductRepository.java
+++ src/main/java/ua/myronets/FirstWebApp/repo/ProductRepository.java
@@ -3,7 +3,11 @@ package ua.myronets.FirstWebApp.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.myronets.FirstWebApp.models.Product;
+import ua.myronets.FirstWebApp.models.Warehouse;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByWarehouse(Warehouse warehouse);
 }
--- src/main/java/ua/myronets/FirstWebApp/repo/WarehouseRepository.java
+++ src/main/java/ua/myronets/FirstWebApp/repo/WarehouseRepository.java
@@ -2,8 +2,12 @@ package ua.myronets.FirstWebApp.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.myronets.FirstWebApp.models.User;
 import ua.myronets.FirstWebApp.models.Warehouse;
+
+import java.util.List;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+    List<Warehouse> findByUser(User user);
 }
