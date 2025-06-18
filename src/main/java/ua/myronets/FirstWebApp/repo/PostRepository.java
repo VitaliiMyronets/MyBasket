9026ce9a695d3ade3dd0937d945e6e45d310fae4
Vitalii Myronets <mironetsvitaly@gmail.com>
2025-06-18 15:28:14 +0300

    added some indentation, blog functionality removed

--- src/main/java/ua/myronets/FirstWebApp/repo/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.myronets.FirstWebApp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ua.myronets.FirstWebApp.models.Post;
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
